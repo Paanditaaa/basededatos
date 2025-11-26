9c5c055277473daf375c85601a0f88c261b3e724
panda <ej755999@gmail.com>
2025-11-25 19:23:18 -0600

    Actualizacion

--- frontend/src/api/users.ts
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const API_URL = "http://localhost:3000"; // Adjust port if necessary
-
-export const getUsers = async () => {
-    const response = await axios.get(`${API_URL}/usuarios`);
-    return response.data;
-};
